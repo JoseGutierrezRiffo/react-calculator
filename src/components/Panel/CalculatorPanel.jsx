05e836c5de91eb19078c3a948e7964a7155a9279
Jose Alejandro Gutierrez Riffo <icce.redes@gmail.com>
2020-09-08 23:29:41 -0300

    first digit validation

--- src/components/Panel/CalculatorPanel.jsx
+++ src/components/Panel/CalculatorPanel.jsx
@@ -4,138 +4,161 @@ import ButtonComponent from "../Button";
 const CalculatorPanel = ({ initialValue }) => {
   const [total, setTotal] = useState(initialValue || "");
 
-  const addValue = e => {
-    setTotal(total + e.target.value);
-  };
-
-  const totalValue = () => {
-    setTotal(eval(total));
-  };
-
   const clearDisplay = () => {
     setTotal("");
   };
 
+  const addValue = e => {
+    let valor = e.target.value;
+    if ((!total && valor === "*") || (!total && valor === "/")) {
+      valor = "";
+      console.log(valor);
+    }
+
+    setTotal(total + valor);
+  };
+
+  const totalValue = e => {
+    e.preventDefault();
+    try {
+      return setTotal(eval(total));
+    } catch (error) {
+      setTotal("");
+    }
+  };
+
   return (
-    <div className="card">
-      <div className="card-body">
-        <div className="row">
-          <div className="col-sm-12">
-            <input type="text" className="form-control text-right" value={total} disabled />
+    <form onSubmit={totalValue}>
+      <div className="card">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-12">
+              <input
+                type="text"
+                className="form-control text-right"
+                value={total}
+                disabled
+              />
+            </div>
+          </div>
+          <div className="row mt-2">
+            <ButtonComponent
+              title={"+"}
+              styleButton={"light"}
+              value={"+"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"-"}
+              styleButton={"light"}
+              value={"-"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"*"}
+              styleButton={"light"}
+              value={"*"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"/"}
+              styleButton={"light"}
+              value={"/"}
+              onClick={addValue}
+            />
+          </div>
+
+          <div className="row mt-2">
+            <ButtonComponent
+              title={"7"}
+              styleButton={"light"}
+              value={"7"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"8"}
+              styleButton={"light"}
+              value={"8"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"9"}
+              styleButton={"light"}
+              value={"9"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              type={"submit"}
+              title={"="}
+              styleButton={"primary"}
+              /*onClick={totalValue}*/
+            />
+          </div>
+
+          <div className="row mt-2">
+            <ButtonComponent
+              title={"4"}
+              styleButton={"light"}
+              value={"4"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"5"}
+              styleButton={"light"}
+              value={"5"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"6"}
+              styleButton={"light"}
+              value={"6"}
+              onClick={addValue}
+            />
+          </div>
+
+          <div className="row mt-2">
+            <ButtonComponent
+              title={"1"}
+              styleButton={"light"}
+              value={"1"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"2"}
+              styleButton={"light"}
+              value={"2"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"3"}
+              styleButton={"light"}
+              value={"3"}
+              onClick={addValue}
+            />
+          </div>
+
+          <div className="row mt-2">
+            <ButtonComponent
+              title={"0"}
+              styleButton={"light"}
+              value={"0"}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"."}
+              styleButton={"light"}
+              value={"."}
+              onClick={addValue}
+            />
+            <ButtonComponent
+              title={"AC"}
+              styleButton={"danger"}
+              onClick={clearDisplay}
+            />
           </div>
         </div>
-        <div className="row mt-2">
-          <ButtonComponent
-            title={"+"}
-            type={"light"}
-            value={"+"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"-"}
-            type={"light"}
-            value={"-"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"x"}
-            type={"light"}
-            value={"x"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"/"}
-            type={"light"}
-            value={"/"}
-            onClick={addValue}
-          />
-        </div>
-
-        <div className="row mt-2">
-          <ButtonComponent
-            title={"7"}
-            type={"light"}
-            value={"7"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"8"}
-            type={"light"}
-            value={"8"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"9"}
-            type={"light"}
-            value={"9"}
-            onClick={addValue}
-          />
-          <ButtonComponent title={"="} type={"primary"} onClick={totalValue} />
-        </div>
-
-        <div className="row mt-2">
-          <ButtonComponent
-            title={"4"}
-            type={"light"}
-            value={"4"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"5"}
-            type={"light"}
-            value={"5"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"6"}
-            type={"light"}
-            value={"6"}
-            onClick={addValue}
-          />
-        </div>
-
-        <div className="row mt-2">
-          <ButtonComponent
-            title={"1"}
-            type={"light"}
-            value={"1"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"2"}
-            type={"light"}
-            value={"2"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"3"}
-            type={"light"}
-            value={"3"}
-            onClick={addValue}
-          />
-        </div>
-
-        <div className="row mt-2">
-          <ButtonComponent
-            title={"0"}
-            type={"light"}
-            value={"0"}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"."}
-            type={"light"}
-            value={"."}
-            onClick={addValue}
-          />
-          <ButtonComponent
-            title={"AC"}
-            type={"danger"}
-            onClick={clearDisplay}
-          />
-        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
